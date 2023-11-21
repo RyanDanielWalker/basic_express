3e17255719503611d97dae03ed78fe2f83df33ac
Ryan Walker <ryandanielwalker@gmail.com>
2023-11-21 12:35:11 -0800

    get + post name

--- myApp.js
+++ myApp.js
@@ -39,4 +39,15 @@ app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word })
 })
 
+app
+  .route("/name")
+  .get((req, res) => {
+    const { first, last } = req.query
+    res.json({ name: `${first} ${last}` })
+  })
+  .post((req, res) => {
+    const { first, last } = req.body
+    res.json({ name: `${first} ${last}` })
+  })
+
 module.exports = app
