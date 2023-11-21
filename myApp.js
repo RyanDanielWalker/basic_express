a0490857d210053ca9f27a370b6b0bd0fe61bf8b
Ryan Walker <ryandanielwalker@gmail.com>
2023-11-21 07:14:53 -0800

    static middleware
    serve json
    
    serve env var to json res
    
    update dependencies
    
    whoops
    
    missing export
    
    here we go again
    
    logs
    
    serve uppercase
    
    add root level middleware
    
    formatter

--- myApp.js
+++ myApp.js
@@ -1,41 +1,27 @@
-let express = require('express');
-let app = express();
-console.log('Hello World');
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
+let express = require("express")
+let app = express()
+require("dotenv").config()
 
+app.use(function (req, res, next) {
+  let method = req.method
+  let path = req.path
+  let ip = req.ip
+  console.log(`${method} ${path} - ${ip}`)
+  next()
+})
 
+app.get("/", function (req, res) {
+  const absPath = __dirname + "/views/index.html"
+  res.sendFile(absPath)
+})
 
+app.use("/public", express.static(__dirname + "/public"))
 
+app.get("/json", (req, res) => {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json",
+  })
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app
